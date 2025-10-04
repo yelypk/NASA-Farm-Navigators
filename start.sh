b68aada8aabdbcff938655bf3a8c71ec0417e4d8
yelyzaveta posheliuk <nemaeimya@gmail.com>
2025-10-05 00:02:03 +0300

    changed bash.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
 set -euo pipefail
 
-if [ -f "front-end/package.json" ]; then
+if [ -f "frontend/package.json" ]; then
   echo "===> Starting FRONTEND (Phaser)"
-  cd front-end
+  cd frontend
   npm ci
   npx http-server -p "${PORT:-3000}" -c-1 .
   exit 0
 fi
 
-if [ -f "back-end/requirements.txt" ]; then
+if [ -f "backend/requirements.txt" ]; then
   echo "===> Starting BACKEND (FastAPI)"
-  cd back-end
+  cd backend
   pip install -r requirements.txt
   uvicorn main:app --host 0.0.0.0 --port "${PORT:-8000}"
   exit 0
